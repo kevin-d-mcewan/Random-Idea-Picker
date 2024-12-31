ba355e6d91b5007b9465c2818b961299fc744ed6
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2024-12-31 14:42:27 -0800

    finished the begging part of this form

---
+++ script.js
@@ -0,0 +1,45 @@
+const tagEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+	createTags(e.target.value);
+
+	if (e.key === "Enter") {
+		setTimeout(() => {
+			e.target.value = "";
+		}, 10);
+
+		randomSelect();
+	}
+});
+
+function randomSelect() {
+	const times = 30;
+
+	const interval = setInterval(() => {
+		const randomTag = pickRandomTag();
+
+		if (randomTag !== undefined) {
+			highlightTag(randomTag);
+
+			setTimeout(() => {
+				unHighlightTag(randomTag);
+			}, 100);
+		}
+	}, times * 100);
+}
+
+function pickRandomTag() {
+	const tags = document.querySelectorAll(".tag");
+	return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+	tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+	tag.classList.remove("highlight");
+}
